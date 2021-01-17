901db50b8d4de3e5d00711db98ab1c0edbaa4477
Victor Dolirio <victor.barbosa@tst.jus.br>
2021-01-16 23:11:51 -0300

    Evolving code.

--- src/Configurator.cpp
+++ src/Configurator.cpp
@@ -13,7 +13,7 @@ void Configurator::saveCredentials(string user, string passwd)
 void Configurator::saveAsLastIp(const string &ip) const
 {
     ofstream file;
-    file.open(this->file, ios::out | ios::binary);
+    file.open(this->file, ios::trunc | ios::binary);
     file << ip;
     file.close();
 }
